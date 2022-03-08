338ca2618f3a5e5371453ac0b4c63fad58577011
minandmin237 <78993150+minandmin237@users.noreply.github.com>
2022-03-08 08:52:42 +0700

    10 Done Add 11
    เพิ่มเนื้อหาบทที่ 10 เพิ่มหัวข้อบทที่ 11

--- part10.py
+++ part10.py
@@ -1,1 +1,15 @@
 #Logical operators
+#AND = และ
+#OR = หรือ
+#NOT = ไม่   
+
+#the answer => True / Fales
+
+x = (5>10) # x value = bool 
+y = (10==5) # y value = bool
+
+# z = (5>10) and (10==5)
+# 5>10 and 10 == 5
+z = (10>5) and (10==5)
+
+print (not z)
